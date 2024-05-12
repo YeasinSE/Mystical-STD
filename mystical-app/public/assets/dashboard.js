eaf43efd263ea58461cdab4914fe6c2ab80d01d4
RiyasaatAhmed <riyasaatahmed24@gmail.com>
2024-05-12 16:40:39 +0600

    feat(mystical-app): next.js project setup #28

--- mystical-app/public/assets/dashboard.js
+++
@@ -1,8 +0,0 @@
-/* globals Chart:false, feather:false */
-
-(function () {
-  'use strict'
-
-  window.feather.replace({ 'aria-hidden': 'true' })
-
-})()
---
+++ mystical-app/src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,4 @@
+import { Button } from "@/components/ui/button";
+export default function SignIn() {
+  return <Button>SignIn Page</Button>;
+}
